e010b4e8f7b365d7e0f680966e1e381fb4762819
tongg-c <tongg.chris@gmail.com>
2023-03-09 16:29:50 -1000

    changed dropdown to hover and changed background color

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const about = document.querySelector('.about');
 const dropDowns = document.querySelectorAll('.dropdown');
 const dropDownMenus = document.querySelectorAll('.dropdown-menu');
 
-document.addEventListener('click', e => {
+document.addEventListener('mouseover', e => {
+  blog.style.backgroundColor = 'inherit';
+  work.style.backgroundColor = 'inherit';
+  about.style.backgroundColor = 'inherit';
   let dropDownMenu;
+
   if (e.target.closest('.dropdown') === blog) {
     dropDownMenu = blog.querySelector('.dropdown-menu');
+    blog.style.backgroundColor = 'rgb(185, 61, 20)';
   }
   if (e.target.closest('.dropdown') === work) {
     dropDownMenu = work.querySelector('.dropdown-menu');
+    work.style.backgroundColor = 'rgb(185, 61, 20)';
   }
   if (e.target.closest('.dropdown') === about) {
     dropDownMenu = about.querySelector('.dropdown-menu');
+    about.style.backgroundColor = 'rgb(185, 61, 20)';
   }
   dropDownMenus.forEach(dropDownMenu => {
     if (
